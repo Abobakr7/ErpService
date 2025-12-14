c53b8f98599db179b734a772241a62bedfe4e45d
Abobakr Osman <mabobakr79@yahoo.com>
2025-12-14 17:51:51 +0200

    feat(controller): add controller and error response dto

--- ErpService/Controllers/InvoiceController.cs
+++ ErpService/Controllers/InvoiceController.cs
@@ -1,6 +1,30 @@
-﻿namespace ErpService.Controllers
+﻿using ErpService.Dtos;
+using ErpService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErpService.Controllers
 {
-    public class InvoiceController
+    [ApiController]
+    [Route("/api/invoices")]
+    public class InvoiceController : ControllerBase
     {
+        private readonly InvoiceService invoiceService;
+
+        public InvoiceController(InvoiceService invoiceService)
+        {
+            this.invoiceService = invoiceService;
+        }
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<ActionResult<InvoiceResponse>> StoreInvoice([FromForm] InvoiceRequest request)
+        {
+            InvoiceResponse? response = await invoiceService.StoreInvoiceAsync(request);
+
+            if (response == null)
+                return Conflict(new ErrorResponse { InvoiceId = request.InvoiceId, Message = "Invoice with this Id already exist." });
+
+            return StatusCode(StatusCodes.Status201Created, response);
+        }
     }
 }
---
+++ ErpService/Dtos/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ErpService.Dtos
+{
+    public sealed class ErrorResponse
+    {
+        public string Message { get; init; } = default!;
+        public Guid InvoiceId { get; init; }
+    }
+}
