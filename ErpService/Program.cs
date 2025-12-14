2915ce78417cfb4cdad3f8de8247666751d18738
Abobakr Osman <mabobakr79@yahoo.com>
2025-12-14 17:52:07 +0200

    refactor: modify main

--- ErpService/Program.cs
+++ ErpService/Program.cs
@@ -1,4 +1,7 @@
 using ErpService.Data;
+using ErpService.Repositories;
+using ErpService.Repositories.Impl;
+using ErpService.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,24 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddScoped<IInvoiceRepository, EFInvoiceRepository>();
+builder.Services.AddScoped<InvoiceService>();
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
