39cb848d2bc7436a36804ac5d4744faa46f8fdd4
Abobakr Osman <mabobakr79@yahoo.com>
2025-12-14 16:23:10 +0200

    feat(data): add db context and entities

--- ErpService/Data/Entities/Invoice.cs
+++ ErpService/Data/Entities/Invoice.cs
@@ -3,33 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ErpService.Data.Entities
 {
+    [Table("Invoices")]
     public class Invoice
     {
-        [Key]
         public Guid InvoiceId { get; set; }
-
         public Guid TicketId { get; set; }
-
-        [MaxLength(9)]
         public string TicketSerial { get; set; } = null!;
-
         public DateTime From { get; set; }
         public DateTime To { get; set; }
         public decimal NumberOfHours { get; set; }
-
-        [MaxLength(7)]
         public string LicensePlate { get; set; } = null!;
-
-        [Column(TypeName = "decimal(18,2)")]
         public decimal AmountWithoutTax { get; set; }
-
-        [Column(TypeName = "decimal(18,2)")]
         public decimal TaxAmount { get; set; }
-
-        [Column(TypeName = "decimal(18,2)")]
         public decimal Amount { get; set; }
-
-        [MaxLength(500)]
-        public string HtmlPath { get; set; } = null!;
+        public byte[] InvoiceHtmlContent { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
